4faa92cb26604e23ae05055b467de662ce1f72eb
edguru <72596185+edguru@users.noreply.github.com>
2021-04-20 17:43:10 +0530

    Update __main__.py

--- __main__.py
+++ __main__.py
@@ -2,10 +2,10 @@ from instadm import InstaDM
 
 if __name__ == '__main__':
 	# Auto login
-	insta = InstaDM(username='your_username', password='your_password', headless=False)
+	insta = InstaDM(username='devi.lcode', password='Devil21', headless=False)
 	
 	# Send message
-	insta.sendMessage(user='username_target', message='Hey !')
+	insta.sendMessage(user='mishragym', message='Hey !')
 	
 	# Send message
-	insta.sendGroupMessage(users=['user1', 'user2'], message='Hey !')
+	#insta.sendGroupMessage(users=['user1', 'user2'], message='Hey !')
